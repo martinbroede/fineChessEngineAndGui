1194a084821982f3d9b9a44513bb4ae946c572c4
martinbroede <martin.broede@yahoo.de>
2021-03-20 15:47:34 +0100

    add quotes

--- ChessUpdateWindows10.bat
+++ ChessUpdateWindows10.bat
@@ -1,11 +1,11 @@
 @echo off
-if NOT exist %~dp0\BLOCK_UPDATE (
+if NOT exist "%~dp0\BLOCK_UPDATE" (
 java -jar fineChess.jar UPDATE
-rd %~dp0\src /s /q
+rd "%~dp0\src" /s /q
 powershell Expand-Archive latestRelease.zip -FORCE
-xcopy %~dp0\latestRelease\fineChessEngineAndGui-main %~dp0 /s /e /h /i /y
-rd %~dp0\latestRelease /s /q
-del %~dp0\latestRelease.zip
+xcopy "%~dp0\latestRelease\fineChessEngineAndGui-main" "%~dp0" /s /e /h /i /y
+rd "%~dp0\latestRelease" /s /q
+del "%~dp0\latestRelease.zip"
 ) ELSE (
 echo You need to delete file BLOCK_UPDATE to update fineChess
 )
