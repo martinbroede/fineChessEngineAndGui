0ea4200f79ea6630a6ca743e61865abdc2501b5c
martinbroede <martin.broede@yahoo.de>
2021-05-16 12:57:22 +0200

    enable font (design) update

--- ChessUpdateWindows10.bat
+++ ChessUpdateWindows10.bat
@@ -2,6 +2,7 @@
 if NOT exist "%~dp0\BLOCK_UPDATE" (
 java -jar fineChess.jar UPDATE
 rd "%~dp0\src" /s /q
+rd "%~dp0\fonts" /s /q
 powershell Expand-Archive latestRelease.zip -FORCE
 xcopy "%~dp0\latestRelease\fineChessEngineAndGui-main" "%~dp0" /s /e /h /i /y
 rd "%~dp0\latestRelease" /s /q
